9fb213b649d4cf7294c9f812586617f18297f3e0
RenatoArenas <desarrollo1@sistemapractico.com>
2022-08-02 12:24:18 -0500

    Rutas modificadas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './pages/Login/Login.js';
 import { Menu } from './pages/Menu/Menu';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/menu' element={<Menu />} />
+            <Route path='/SistemaPractico-Login-ReactJs' element={<Login />} />
+            <Route path='/SistemaPractico-Login-ReactJs/menu' element={<Menu />} />
         </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
