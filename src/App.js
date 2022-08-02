edec67ed092c82b1a6283e832a4b27a409d8b1da
RenatoArenas <desarrollo1@sistemapractico.com>
2022-08-02 12:34:17 -0500

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './pages/Login/Login.js';
 import { Menu } from './pages/Menu/Menu';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route path='/SistemaPractico-Login-ReactJs' element={<Login />} />
             <Route path='/SistemaPractico-Login-ReactJs/menu' element={<Menu />} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
