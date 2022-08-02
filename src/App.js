7880597505be42fa73092c6e9e6b4963fa92fe68
RenatoArenas <desarrollo1@sistemapractico.com>
2022-08-02 09:31:30 -0500

    HashRouter para GithubPages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './pages/Login/Login.js';
 import { Menu } from './pages/Menu/Menu';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route path='/' element={<Login />} />
             <Route path='/menu' element={<Menu />} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
